c7ee3d19159c7f5ce44d1af86b6169c405c190b3
NathanLiew214 <66583135+NathanLiew214@users.noreply.github.com>
2020-06-08 08:32:38 -0700

    Update hello.c

--- hello.c
+++ hello.c
@@ -1,9 +1,23 @@
+/*
+*Author: Andrew Lunde
+*Date 2020/06/08
+*Collaborators: Nathan Liew
+*Email : wliew@unl.edu
+*A simple hello world program in C
+*/
 
 
+#include <stdlib.h>
 #include <stdio.h>
-int main(void){
+
+int main (int argc, char **argv){
   printf("Andrew Lunde\n");
-  printf("Computer Engineering\n");
+  printf("Computer Engineering\n\n");
+  
+  printf("Nathan Liew\n");
+  printf("Computer Science\n");
+  
+  
 
   return 0;
 }
